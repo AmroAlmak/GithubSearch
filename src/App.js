f48295534817f0df962101eea33538e4a4af7cd5
Amro Almak <amroelsir123@gmail.com>
2023-08-18 18:08:29 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [githubUser, setGithubUser] = useState({});
+  const [repositories, setRepositories] = useState([]);
+  const [userName, setUserName] = useState("");
+  const [input, setInput] = useState("");
+  const [error, setError] = useState(null);
+
+  // Fetching user data from Github API
+  const fetchUserData = async () => {
+    if (!userName) {
+      return;
+    }
+    try {
+      const response = await fetch(`https://api.github.com/users/${userName}`);
+      if (!response.ok) {
+        setError("User not found");
+      } else {
+        setError(null);
+        const data = await response.json();
+        console.log(data);
+        setGithubUser(data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // Fetching repositories data from Github API
+  const fetchRepositories = async () => {
+    if (!userName) {
+      return;
+    }
+    try {
+      const response = await fetch(
+        `https://api.github.com/users/${userName}/repos`
+      );
+      if (!response.ok) {
+        setError("user not found");
+      } else {
+        const data = await response.json();
+        setRepositories(data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+    fetchRepositories();
+  }, [userName]);
+
+  console.log(githubUser);
+  console.log(repositories);
+
+  // Handling input
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  // Handling submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUserName(input);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleInput}
+          type="text"
+          placeholder="Enter GitHub username"
+          value={input}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {Object.keys(githubUser).length > 0 && (
+        <div className="githubUser-card">
+          {githubUser.avatar_url && (
+            <img src={githubUser.avatar_url} alt="avatar" />
+          )}
+          <h3>User Name: {githubUser.name}</h3>
+          <p>{githubUser.bio}</p>
+          <p>Number of Public Repositories: {githubUser.public_repos}</p>
+          <p>Followers: {githubUser.followers}</p>
+          <p>Following: {githubUser.following}</p>
+          <h2>Repositories</h2>
+          <ul>
+            {repositories?.map((repo) => (
+              <li key={repo.id}>
+                <strong>{repo.name}</strong> {repo.description}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {error && <h1>{error}</h1>}
     </div>
   );
 }
